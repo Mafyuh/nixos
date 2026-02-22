6903de6633848fea3d68c0150ec9e52d75a3b357
Mafyuh <admin@mafyuh.dev>
2026-02-21 21:02:29 -0500

    fix: downgrade kernel package to linuxPackages_6_12 for compatibility

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -65,7 +65,7 @@
   };
 
   # renovate: depName=linux datasource=repology versioning=loose
-  boot.kernelPackages = pkgs.linuxPackages_6_13;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = false;
