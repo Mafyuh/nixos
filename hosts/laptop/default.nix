778ce4ec2692a91005f132d059b6c730cdc6b86f
Mafyuh <admin@mafyuh.dev>
2026-02-21 21:00:40 -0500

    feat: add Renovate configuration for automatic Linux kernel updates

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -64,6 +64,8 @@
     pulse.enable = true;
   };
 
+  # renovate: depName=linux datasource=repology versioning=loose
+  boot.kernelPackages = pkgs.linuxPackages_6_13;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = false;
