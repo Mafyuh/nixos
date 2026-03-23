85853641f6caa6ff8cc9648f70da78be5b83f243
Mafyuh <admin@mafyuh.dev>
2026-03-22 21:32:43 -0400

    chore: remove explicit grub device path

--- hosts/racknerd-vps/boot.nix
+++ hosts/racknerd-vps/boot.nix
@@ -2,7 +2,6 @@
   boot.initrd.availableKernelModules = [ "virtio_pci" "virtio_scsi" "ahci" "sd_mod" ];
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.efiSupport = false;
   boot.loader.systemd-boot.enable = false;
 }
