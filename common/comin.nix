f3f3f307111a7ed6091739e5ae3ea385fb5bc9fd
Mafyuh <matt@mafyuh.com>
2026-05-09 11:46:25 -0400

    feat: add comin exporter

--- common/comin.nix
+++ common/comin.nix
@@ -1,6 +1,11 @@
 { config, ... }: {
   services.comin = {
     enable = true;
+    exporter = {
+      listen_address = "0.0.0.0";
+      openFirewall = false;
+      port = 4243;
+    };
     remotes = [{
       name = "origin";
       url = "https://github.com/Mafyuh/nixos.git";
